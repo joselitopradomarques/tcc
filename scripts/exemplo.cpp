45eee3aa2100d3eafbbaf3651e276377eb682ec7
Joselito Prado <joselito.prado.marques@gmail.com>
2024-12-09 17:16:23 -0300

    Arquivos da pasta script limpos

--- scripts/exemplo.cpp
+++
@@ -1,8 +0,0 @@
-#include <liquid/liquid.h>
-#include <iostream>
-
-int main() {
-    // Exemplo simples de uso do LiquidDSP
-    std::cout << "LiquidDSP está instalado e funcionando!" << std::endl;
-    return 0;
-}
