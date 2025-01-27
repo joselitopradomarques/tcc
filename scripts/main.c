2b9ed433ead91a9fc82ed99b5d6a55ddfdb2e578
Joselito Rasp <joselito.prado.marques@gmail.com>
2025-01-27 20:16:16 -0300

    buffer circular com thread

--- scripts/main.c
+++ scripts/main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <alsa/asoundlib.h>
+#include <pthread.h>
 #include "capture.h"
 
 // Declare a constante externa UPDATE_BUFFER_SIZE
 extern const size_t UPDATE_BUFFER_SIZE;
+
+typedef struct {
+    snd_pcm_t *pcm_handle;
+    CircularBuffer *write_buffer;
+    CircularBuffer *read_buffer;
+} CaptureThreadArgs;
+
+// Função para capturar áudio em uma thread
+void *capture_audio_thread(void *args) {
+    CaptureThreadArgs *capture_args = (CaptureThreadArgs *)args;
+    snd_pcm_t *pcm_handle = capture_args->pcm_handle;
+    CircularBuffer *write_buffer = capture_args->write_buffer;
+    CircularBuffer *read_buffer = capture_args->read_buffer;
+
+    capture_audio_loop(pcm_handle, write_buffer, read_buffer);
+
+    return NULL;
+}
 
 int main() {
     snd_pcm_t *pcm_handle;
@@ -24,8 +43,24 @@ int main() {
         return -1;
     }
 
-    // Captura áudio em loop
-    capture_audio_loop(pcm_handle, &write_buffer, &read_buffer);
+    // Estrutura para passar parâmetros para a thread
+    CaptureThreadArgs capture_args;
+    capture_args.pcm_handle = pcm_handle;
+    capture_args.write_buffer = &write_buffer;
+    capture_args.read_buffer = &read_buffer;
+
+    // Criação da thread de captura de áudio
+    pthread_t capture_thread;
+    if (pthread_create(&capture_thread, NULL, capture_audio_thread, &capture_args) != 0) {
+        fprintf(stderr, "Erro ao criar a thread de captura de áudio\n");
+        return -1;
+    }
+
+    // Aguardar a thread de captura terminar
+    if (pthread_join(capture_thread, NULL) != 0) {
+        fprintf(stderr, "Erro ao esperar a thread de captura de áudio\n");
+        return -1;
+    }
 
     // Fecha captura e libera buffers
     close_capture(pcm_handle, &write_buffer, &read_buffer);
